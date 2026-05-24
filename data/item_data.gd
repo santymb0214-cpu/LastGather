9ef000727263b7056a9570a6b7dd79572eeb520a
PisoCreo <pisopisado6112@gmail.com>
2026-05-24 00:57:53 -0600

    Cambios Globales

--- data/item_data.gd
+++ data/item_data.gd
@@ -1,2 +1,12 @@
 extends Item
 class_name ItemData
+
+enum Type {
+	COMIDA, MEDICAMENTOS, SCROLL, EQUIPAMIENTO
+}
+
+
+@export var type: Type
+@export var can_drop: bool
+@export var is_consumible: bool
+@export var max_stack: int = 999
